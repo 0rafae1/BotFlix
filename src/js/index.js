4a8822ac67252447c89cdb4c08e4ca644a907161
Rafael Sousa <ffael.sousa1@gmail.com>
2025-09-07 20:22:58 -0300

    Ajustado visualização mobile

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,16 @@ let searchButton;
 
 document.addEventListener("DOMContentLoaded", () => {
     moodTextArea = document.getElementById("mood-textarea");
-    console.log(moodTextArea);
     searchButton = document.getElementById("search-button");
     setupEventListeners();
+    const backButton = document.getElementById("back-button");
+    if (backButton) {
+        backButton.addEventListener("click", () => {
+            document.querySelector(".results").classList.remove("show-on-mobile");
+            document.querySelector(".search-card").classList.remove("hide-on-mobile");
+            backButton.style.display = "none";
+        });
+    }
 });
 
 function setupEventListeners() {
@@ -44,21 +51,25 @@ async function handleSearch() {
             const resultsDiv = document.getElementById('results');
             const moviesGrid = document.getElementById('movies-grid');
 
-            resultsDiv.classList.add("show")
-            
+            resultsDiv.classList.add("show");
+            if (window.innerWidth <= 600) {
+                resultsDiv.classList.add("show-on-mobile");
+                document.querySelector(".search-card").classList.add("hide-on-mobile");
+                document.getElementById("back-button").style.display = "block";
+            }
             moviesGrid.innerHTML = `
-                    <div class="movie-card">
-                        <div class="movie-poster">
-                            ${posterUrl ? `<img src="${posterUrl}" alt="${movie.title}">` : '<div class="no-poster">Sem imagem</div>'}
-                        </div>
-                        <div class="movie-info">
-                            <h4 class="movie-title">${movie.title}</h4>
-                            <p class="movie-overview">${movie.overview || 'Sem descrição disponível.'}</p>
-                            <p class="movie-rating">⭐ ${movie.vote_average.toFixed(1)} / 10</p>
-                        </div>
+                <div class="movie-card">
+                    <div class="movie-poster">
+                        ${posterUrl ? `<img src="${posterUrl}" alt="${movie.title}">` : '<div class="no-poster">Sem imagem</div>'}
                     </div>
-                `;
-           }
+                    <div class="movie-info">
+                        <h4 class="movie-title">${movie.title}</h4>
+                        <p class="movie-overview">${movie.overview || 'Sem descrição disponível.'}</p>
+                        <p class="movie-rating">⭐ ${movie.vote_average.toFixed(1)} / 10</p>
+                    </div>
+                </div>
+            `;
+        }
 
 
 }
